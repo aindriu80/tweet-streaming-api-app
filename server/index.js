4c12489f3d0f7dfee3d3ee9d942aee6b7899d197
aindriu80 <aindriu80@gmail.com>
2021-01-02 22:38:38 +0000

    Layout fix

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const rulesURL = "https://api.twitter.com/2/tweets/search/stream/rules";
 const streamURL =
   "https://api.twitter.com/2/tweets/search/stream?tweet.fields=public_metrics&expansions=author_id";
 
-const rules = [{ value: "gaeilge" }];
+const rules = [{ value: "Gaeilge" }];
 
 const app = express();
 
